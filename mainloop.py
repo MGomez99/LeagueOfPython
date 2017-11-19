76c94c0b8eefe8b7a0904ec9bdcd3d7034f7d486
tomwitz355 <32752723+tomwitz355@users.noreply.github.com>
2017-11-18 19:10:32 -0500

    Add files via upload
    This is a wip, as we have a few design choices we have to make, but here is a general design for the main loop

---
+++ mainloop.py
@@ -0,0 +1,87 @@
+import pygame
+import player1
+import player2
+import sys
+
+class Controller:
+	def __init__(self, width= , height= ,player1_spec , player2_spec  )
+	pygame.init()
+	self.width = width
+	self.height = height
+	self.screen = pygame.display.set_mode((self.width, self.height))
+	self.background =pygame.Surface(self.screen.get_size())
+	self.player1 = player1_spec
+	self.player2 = player2_spec		
+	"""
+	self.minions = []
+	self.minions.append(minions.Minions(spawn))"""
+	#self.tower1(spawn)
+	#self.tower2(spawn)
+	self.sprites = pygame.sprite.Group((self.player1,) + (self.player2,) """+ tuple(self.minions) + (self.tower1,) + (self.tower2,)""")
+
+	def mainLoop(self):
+		pygame.key.set_repeat(1,60)
+		while True:
+			for event in pygame.event.get():
+				if even.type == pygame.Quit:
+					sys.exit()
+				if event.type == pygame.KEYDOWN:
+					if(event.key == pygame.K_UP):
+                        			self.player1.move_up()
+                   			if(event.key == pygame.K_DOWN):
+                        			self.player1.move_down()
+                    			if(event.key == pygame.K_LEFT):
+                        			self.player1.move_left()
+                   			if(event.key == pygame.K_RIGHT):
+                        			self.player1.move_right()
+					if(event.key == pygame.K_KP1):
+						self.player1.attack1()
+					if(event.key == pygame.K_KP2):
+						self.player1.attack2()
+					if(event.key == pygame.K_KP3):
+						self.player1.attack3()
+					if human == True
+						if(event.key == pygame.K_w):
+							self.player2.move_up()
+						if(event.key == pygame.K_s):
+							self.player2.move_down()
+						if(event.key == pygame.K_a):
+							self.player2.move_left()
+						if(event.key == pygame.K_d):
+							self.player2.move_right()
+						if(event.key == pygame.K_y):
+							self.player2.attack1()
+						if(event.key == pygame.K_u):
+							self.player2.attack2()
+						if(event.key == pygame.K_i):
+							self.player2.attack3()
+			if(pygame.sprite.collide_rect(self.player2.attack, self.player1):
+				self.player1.health -= self.player2.attack.damage()
+			#copy and paste this for all the attacks or maybe we can for loop it
+			if(self.player1.health == 0):
+				self.player1.kill()
+			if(self.player2.health == 0):
+				self.player2.kill()
+			self.screen.blit(self.background, (0,0))
+			self.sprites.draw(self.screen)
+			pygame.display.flip()
+
+def main():
+	spec_choice_1 = input("Choose a spec for player 1 (name of specs here): ")
+	name1 = input("what is your name? ")
+	if spec_choice_1 == "name of spec 1"
+		spec_choice_1 = spec.Spec1
+		player1_spec = player1_spec.Player1(spec_choice_1, spawn, name1)
+	"""same for the other specs"""
+	human = input("is player 2 human or computer controlled (y/n)? ")
+	if human == "y":
+		human = True
+	else:
+		human = False
+	spec_choice_2 = input("Choose a spec for player 2 (name of specs here): ")
+	name2 = input("what is your name? ")
+	if spec_choice_2 == "name of spec 1"
+		spec_choice_2 = spec.Spec1
+		player2_spec = player1_spec.Player1(spec_choice_1, spawn, human, name1)
+	gameon = Controller(player1_spec, player2_spec)
+	gameon.mainLoop()
