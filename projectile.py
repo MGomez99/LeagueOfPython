89d9fe436d6ac885706245ef63acd14bf583bf44
POXZASY <32286007+POXZASY@users.noreply.github.com>
2017-11-27 22:04:26 -0500

    Add files via upload

---
+++ projectile.py
@@ -0,0 +1,27 @@
+import pygame
+
+
+class Projectile(pygame.sprite.Sprite):
+    res=pygame.display.get_surface().get_size()
+    def __init__(self, xcoor, ycoor, direction, velocity, damage, team, image):
+        pygame.sprite.Sprite.__init__(self)
+        self.x=xcoor
+        self.y=ycoor
+        self.dir=direction
+        self.vel=velocity
+        self.dmg=damage
+        self.team=team #BLUE (left side) RED (right side)
+        self.projectile=pygame.image.load(image)
+        self.rect=self.projectile.get_rect()
+    def update(self, player): 
+        if self.team=="BLUE" and self.x<res[0]:
+            self.x+=self.vel
+        elif self.team == "RED" and self.x>res[0]:
+            self.x-=self.vel
+        elif self.team=="BLUE" and self.x>=res[0]:
+            self.kill()
+        elif self.team=="RED" and self.x<=0:
+            self.kill()
+        if self.team != player.team:
+            if(self.rect.colliderect(player.rect))==True:
+                player.health=player.health-self.damage
