62d8e6aa63c8456ff5a7408b9cc8eaeba17c7b87
tomwitz355 <thorowi1@binghamton.edu>
2017-12-02 14:43:03 -0500

    hello there General Kenobi

---
+++ menu_screen.py
@@ -0,0 +1,7 @@
+class Menu_screen:
+	def __init__(self, width, height, bg_color = (0,0,0)):
+		self.width = width
+		self.height = height
+		self.screen = pygame.display.set_mode((self.width, self.height))
+		self.background = pygame.Surface(self.screen.get_size())
+
