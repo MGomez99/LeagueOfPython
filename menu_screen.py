80210c4c4b5b8368d9384b9b9fea3090898c3dab
Unknown <xmgomez2012@gmail.com>
2017-12-02 15:59:19 -0500

    Auto stash before merge of "master" and "origin/master"

--- menu_screen.py
+++ menu_screen.py
@@ -2,8 +2,9 @@ import pygame
 
 
 class Menu_screen:
-    def __init__(self, width, height, bg_color=(0, 0, 0)):
+    def __init__(self, width, height, bg_color = (255, 255, 255)):
         self.width = width
         self.height = height
         self.screen = pygame.display.set_mode((self.width, self.height))
         self.background = pygame.Surface(self.screen.get_size())
+        self.background
