f64a8a0438386be26ea1a6f6ac83061b1f287940
POXZASY <larkin9w@gmail.com>
2017-12-02 14:18:47 -0500

    prelim update

--- projectile.py
+++ projectile.py
@@ -3,11 +3,10 @@ import pygame
 
 class Projectile(pygame.sprite.Sprite):
     res=pygame.display.get_surface().get_size()
-    def __init__(self, xcoor, ycoor, direction, velocity, damage, team, image):
+    def __init__(self, xcoor, ycoor, velocity, damage, team, image):
         pygame.sprite.Sprite.__init__(self)
         self.x=xcoor
         self.y=ycoor
-        self.dir=direction
         self.vel=velocity
         self.dmg=damage
         self.team=team #BLUE (left side) RED (right side)
@@ -20,8 +19,9 @@ class Projectile(pygame.sprite.Sprite):
             self.x-=self.vel
         elif self.team=="BLUE" and self.x>=res[0]:
             self.kill()
-        elif self.team=="RED" and self.x<=0:
+        elif self.team=="RED" and self.x<=res[0]:
             self.kill()
         if self.team != player.team:
             if(self.rect.colliderect(player.rect))==True:
                 player.health=player.health-self.damage
+                self.kill()
