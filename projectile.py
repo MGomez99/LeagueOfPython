198bdaa4db3c02b842cd36b5e5d68cc7f3c71cfe
Unknown <xmgomez2012@gmail.com>
2017-12-02 14:54:12 -0500

    Auto stash before merge of "master" and "origin/master"

--- projectile.py
+++ projectile.py
@@ -2,26 +2,28 @@ import pygame
 
 
 class Projectile(pygame.sprite.Sprite):
-    res=pygame.display.get_surface().get_size()
+    res = pygame.display.get_surface().get_size()
+
     def __init__(self, xcoor, ycoor, velocity, damage, team, image):
         pygame.sprite.Sprite.__init__(self)
-        self.x=xcoor
-        self.y=ycoor
-        self.vel=velocity
-        self.dmg=damage
-        self.team=team #BLUE (left side) RED (right side)
-        self.projectile=pygame.image.load(image)
+        self.x = xcoor
+        self.y = ycoor
+        self.vel = velocity
+        self.dmg = damage
+        self.team = team  # BLUE (left side) RED (right side)
+        self.projectile = pygame.image.load(image)
         self.rect=self.projectile.get_rect()
+
     def update(self, player): 
-        if self.team=="BLUE" and self.x<res[0]:
-            self.x+=self.vel
+        if self.team == "BLUE" and self.x<res[0]:
+            self.x += self.vel
         elif self.team == "RED" and self.x>res[0]:
-            self.x-=self.vel
-        elif self.team=="BLUE" and self.x>=res[0]:
+            self.x -= self.vel
+        elif self.team == "BLUE" and self.x>=res[0]:
             self.kill()
-        elif self.team=="RED" and self.x<=res[0]:
+        elif self.team == "RED" and self.x<=res[0]:
             self.kill()
         if self.team != player.team:
-            if(self.rect.colliderect(player.rect))==True:
-                player.health=player.health-self.damage
+            if self.rect.colliderect(player.rect):
+                player.health = player.health-self.damage
                 self.kill()
