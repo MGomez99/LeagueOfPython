280ea6e879a9858bf1147304a09e34648bf8281a
Unknown <xmgomez2012@gmail.com>
2017-12-02 20:58:51 -0500

    jfc

--- mainloop.py
+++ mainloop.py
@@ -31,58 +31,60 @@ class Controller:
                     Controller.player1select()
                     break
 
-        def player1select(self):
-            self.background.blit("player1menu.png", (0, 0))
-            for event in pygame.event.get():
-                if event.type==pygame.KEYDOWN:
-                    if event.key==K_q:
-                        player1=player.player(50,self.height/2,"BLUE",spec1)
-                        Controller.player2select()
-                        break
-                    elif event.key==K_w:
-                        player1=player.player(50,self.height/2,"BLUE",spec2)
-                        Controller.player2select()
-                        break
-                    elif event.key==K_e:
-                        player1=player.player(50,self.height/2,"BLUE",spec3)
-                        Controller.player2select()
-                        break
-        def player2select(self):
-            self.background.blit("player2menu.png", (0, 0))
-            for event in pygame.event.get():
-                if event.type == pygame.KEYDOWN:
-                    if event.key == K_u:
-                        player2 = player.player(self.width-50, self.height / 2, "RED", spec1)
-                        Controller.mainLoop()
-                        break
-                    elif event.key == K_i:
-                        player2 = player.player(self.width-50, self.height / 2, "RED", spec2)
-                        Controller.mainLoop()
-                        break
-                    elif event.key == K_o:
-                        player2 = player.player(self.width-50, self.height / 2, "RED", spec3)
-                        Controller.mainLoop()
-                        break
-        def mainLoop(self):
-            pygame.key.set_repeat(1, 60)
-            sprites.add(player1)
-            sprites.add(player2)
-            while True:
-                player1.update()
-                player2.update()
-                if len(bullets.sprites()>0):
-                    bullet.update() #will need to update for uniques as well
-                for event in pygame.event.get():
-                    if event.type == pygame.Quit:
-                        sys.exit()
-                    if event.type == pygame.KEYDOWN:
-                        if event.key == K_ESCAPE:
-                            pause = True
-                            pause_menu.paused()
+    def player1select(self):
+        self.background.blit("player1menu.png", (0, 0))
+        for event in pygame.event.get():
+            if event.type==pygame.KEYDOWN:
+                if event.key==K_q:
+                    player1=player.player(50,self.height/2,"BLUE",spec1)
+                    Controller.player2select()
+                    break
+                elif event.key==K_w:
+                    player1=player.player(50,self.height/2,"BLUE",spec2)
+                    Controller.player2select()
+                    break
+                elif event.key==K_e:
+                    player1=player.player(50,self.height/2,"BLUE",spec3)
+                    Controller.player2select()
+                    break
+                    
+    def player2select(self):
+        self.background.blit("player2menu.png", (0, 0))
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == K_u:
+                    player2 = player.player(self.width-50, self.height / 2, "RED", spec1)
+                    Controller.mainLoop()
+                    break
+                elif event.key == K_i:
+                    player2 = player.player(self.width-50, self.height / 2, "RED", spec2)
+                    Controller.mainLoop()
+                    break
+                elif event.key == K_o:
+                    player2 = player.player(self.width-50, self.height / 2, "RED", spec3)
+                    Controller.mainLoop()
+                    break
 
-                self.screen.blit(self.background, (0, 0))
-                self.sprites.draw(self.screen)
-                pygame.display.flip()
+    def mainLoop(self):
+        pygame.key.set_repeat(1, 60)
+        sprites.add(player1)
+        sprites.add(player2)
+        while True:
+            player1.update()
+            player2.update()
+            if len(bullets.sprites()>0):
+                bullet.update() #will need to update for uniques as well
+            for event in pygame.event.get():
+                if event.type == pygame.Quit:
+                    sys.exit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == K_ESCAPE:
+                        pause = True
+                        pause_menu.paused()
+
+            self.screen.blit(self.background, (0, 0))
+            self.sprites.draw(self.screen)
+            pygame.display.flip()
 
 
 def main():
