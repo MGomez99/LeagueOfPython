83e057abe2ad6f115b4f5d39133d1de1dfaa3dc6
Unknown <lwisdom1@binghamton.edu>
2017-12-06 23:57:24 -0500

    actual final

--- Stats.py
+++ Stats.py
@@ -3,8 +3,8 @@ import json
 
 
 def show_stats(player1, player2, screen):
-    textsize1=9 #regular stats
-    textsize2=27 #headers
+    textsize1 = 9  # regular stats
+    textsize2 = 27  # headers
     """
     Some Larkin magic that shows stats. Player dependent stat tracking and placement
     :param player1: player 1
@@ -20,29 +20,29 @@ def show_stats(player1, player2, screen):
     screen.blit(game_over_bg, game_over_bg.get_rect())
 
     # statistics
-    title1_text = pygame.font.Font('assets/spaceage.ttf', textsize2).render("PLAYER 1", True, (255,255,255))
+    title1_text = pygame.font.Font('assets/spaceage.ttf', textsize2).render("PLAYER 1", True, (255, 255, 255))
     title1_rect = title1_text.get_rect(left=100, top=350)
     screen.blit(title1_text, title1_rect)
-    title2_text = pygame.font.Font('assets/spaceage.ttf', textsize2).render("PLAYER 2", True, (255,255,255))
+    title2_text = pygame.font.Font('assets/spaceage.ttf', textsize2).render("PLAYER 2", True, (255, 255, 255))
     title2_rect = title2_text.get_rect(left=500, top=350)
     screen.blit(title2_text, title2_rect)
 
     # number of shots
     numofshots1_text = pygame.font.Font('assets/spaceage.ttf', textsize1).render(
-        "Number of shots: " + str(player1.number_of_shots), True, (255,255,255))
+        "Number of shots: " + str(player1.number_of_shots), True, (255, 255, 255))
     numofshots1_rect = numofshots1_text.get_rect(left=100, top=430)
     screen.blit(numofshots1_text, numofshots1_rect)
     numofshots2_text = pygame.font.Font('assets/spaceage.ttf', textsize1).render(
-        "Number of shots: " + str(player2.number_of_shots), True, (255,255,255))
+        "Number of shots: " + str(player2.number_of_shots), True, (255, 255, 255))
     numofshots2_rect = numofshots2_text.get_rect(left=500, top=430)
     screen.blit(numofshots2_text, numofshots2_rect)
     # number of hits
     numofhits1_text = pygame.font.Font('assets/spaceage.ttf', textsize1).render(
-        "Number of hits: " + str(player1.number_of_hits), True, (255,255,255))
+        "Number of hits: " + str(player1.number_of_hits), True, (255, 255, 255))
     numofhits1_rect = numofhits1_text.get_rect(left=100, top=450)
     screen.blit(numofhits1_text, numofhits1_rect)
     numofhits2_text = pygame.font.Font('assets/spaceage.ttf', textsize1).render(
-        "Number of hits: " + str(player2.number_of_hits), True, (255,255,255))
+        "Number of hits: " + str(player2.number_of_hits), True, (255, 255, 255))
     numofhits2_rect = numofhits2_text.get_rect(left=500, top=450)
     screen.blit(numofhits2_text, numofhits2_rect)
 
@@ -55,43 +55,45 @@ def show_stats(player1, player2, screen):
         acc2 = str(round(player2.number_of_hits / player2.number_of_shots, 2) * 100) + "%"
     else:
         acc2 = "N/A"
-    acc1_text = pygame.font.Font('assets/spaceage.ttf', textsize1).render("Accuracy: " + acc1, True, (255,255,255))
+    acc1_text = pygame.font.Font('assets/spaceage.ttf', textsize1).render("Accuracy: " + acc1, True, (255, 255, 255))
     acc1_rect = acc1_text.get_rect(left=100, top=470)
     screen.blit(acc1_text, acc1_rect)
-    acc2_text = pygame.font.Font('assets/spaceage.ttf', textsize1).render("Accuracy: " + acc2, True, (255,255,255))
+    acc2_text = pygame.font.Font('assets/spaceage.ttf', textsize1).render("Accuracy: " + acc2, True, (255, 255, 255))
     acc2_rect = acc2_text.get_rect(left=500, top=470)
     screen.blit(acc2_text, acc2_rect)
 
     # damage taken
     dmg1_text = pygame.font.Font('assets/spaceage.ttf', textsize1).render("Damage taken: " + str(player1.damagetaken),
-                                                                   True, (255,255,255))
+                                                                          True, (255, 255, 255))
     dmg1_rect = dmg1_text.get_rect(left=100, top=490)
     screen.blit(dmg1_text, dmg1_rect)
     dmg2_text = pygame.font.Font('assets/spaceage.ttf', textsize1).render("Damage taken: " + str(player2.damagetaken),
-                                                                   True, (255,255,255))
+                                                                          True, (255, 255, 255))
     dmg2_rect = dmg2_text.get_rect(left=500, top=490)
     screen.blit(dmg2_text, dmg2_rect)
 
     # mana spent
     mana1_text = pygame.font.Font('assets/spaceage.ttf', textsize1).render("Mana spent: " + str(player1.manaspent),
-                                                                    True, (255,255,255))
+                                                                           True, (255, 255, 255))
     mana1_rect = mana1_text.get_rect(left=100, top=510)
     screen.blit(mana1_text, mana1_rect)
     mana2_text = pygame.font.Font('assets/spaceage.ttf', textsize1).render("Mana spent: " + str(player2.manaspent),
-                                                                    True, (255,255,255))
+                                                                           True, (255, 255, 255))
     mana2_rect = mana1_text.get_rect(left=500, top=510)
     screen.blit(mana2_text, mana2_rect)
 
     # final health
     hp1_text = pygame.font.Font('assets/spaceage.ttf', textsize1).render("Final health: " + str(player1.hp), True,
-                                                                  (255,255,255))
+                                                                         (255, 255, 255))
     hp1_rect = hp1_text.get_rect(left=100, top=530)
     screen.blit(hp1_text, hp1_rect)
     hp2_text = pygame.font.Font('assets/spaceage.ttf', textsize1).render("Final health: " + str(player2.hp), True,
-                                                                  (255,255,255))
+                                                                         (255, 255, 255))
     hp2_rect = hp2_text.get_rect(left=500, top=530)
     screen.blit(hp2_text, hp2_rect)
     pygame.display.flip()
+
+
 def highScores(player, screen):
     """
         Displays high scores stored externally
@@ -99,61 +101,61 @@ def highScores(player, screen):
         :param screen: pygame screen
         :return: none
     """
-    data={}
-    textsize1=9 #regular stats
-    textsize2=27 #headers
+    data = {}
+    textsize1 = 9  # regular stats
+    textsize2 = 27  # headers
     with open("highscores.txt", "r") as file:
-        data=json.load(file)
+        data = json.load(file)
     with open("highscores.txt", "w") as file:
-        statistics=["number_of_shots", "number_of_hits", "accuracy", "damagetaken", "manaspent", "hp"]
-        n=0
+        statistics = ["number_of_shots", "number_of_hits", "accuracy", "damagetaken", "manaspent", "hp"]
+        n = 0
         for stat in statistics:
             if stat in data.keys():
-                if n==0:
+                if n == 0:
                     if int(data["number_of_shots"]) < int(player.number_of_shots):
                         data["number_of_shots"] = player.number_of_shots
-                    n+=1
-                if n==1:
+                    n += 1
+                if n == 1:
                     if int(data["number_of_hits"]) < int(player.number_of_hits):
                         data["number_of_hits"] = player.number_of_hits
-                    n+=1
-                if n==2:
+                    n += 1
+                if n == 2:
 
                     if int(data["accuracy"]) < int(player.accuracy):
                         data["accuracy"] = player.accuracy
-                    n+=1
-                if n==3:
+                    n += 1
+                if n == 3:
                     if int(data["damagetaken"]) < int(player.damagetaken):
                         data["damagetaken"] = player.damagetaken
                     n += 1
-                if n==4:
+                if n == 4:
                     if int(data["manaspent"]) < int(player.manaspent):
                         data["manaspent"] = player.manaspent
                     n += 1
-                if n==5:
+                if n == 5:
                     if int(data["hp"]) < int(player.hp):
                         data["hp"] = player.hp
                     n += 1
 
             else:
-                if n==0:
+                if n == 0:
                     data["number_of_shots"] = player.number_of_shots
-                    n+=1
-                if n==1:
+                    n += 1
+                if n == 1:
                     data["number_of_hits"] = player.number_of_hits
-                    n+=1
-                if n==2:
+                    n += 1
+                if n == 2:
                     data["accuracy"] = player.accuracy
-                    n+=1
-                if n==3:
+                    n += 1
+                if n == 3:
                     data["damagetaken"] = player.damagetaken
                     n += 1
-                if n==4:
+                if n == 4:
                     data["manaspent"] = player.manaspent
                     n += 1
-                if n==5:
+                if n == 5:
                     data["hp"] = player.hp
-        jsonstuff=json.dumps(data)
+        jsonstuff = json.dumps(data)
         file.write(jsonstuff)
 
     # header
@@ -174,25 +176,26 @@ def highScores(player, screen):
     screen.blit(numofhits_text, numofhits_rect)
 
     # accuracy
-    acc_text = pygame.font.Font('assets/spaceage.ttf', textsize1).render("Accuracy: " + str(data["accuracy"]), True, (255, 255, 255))
+    acc_text = pygame.font.Font('assets/spaceage.ttf', textsize1).render("Accuracy: " + str(data["accuracy"]), True,
+                                                                         (255, 255, 255))
     acc_rect = acc_text.get_rect(left=300, top=470)
     screen.blit(acc_text, acc_rect)
 
     # damage taken
     dmg_text = pygame.font.Font('assets/spaceage.ttf', textsize1).render("Damage taken: " + str(data["damagetaken"]),
-                                                                   True, (255, 255, 255))
+                                                                         True, (255, 255, 255))
     dmg_rect = dmg_text.get_rect(left=300, top=490)
     screen.blit(dmg_text, dmg_rect)
 
     # mana spent
     mana_text = pygame.font.Font('assets/spaceage.ttf', textsize1).render("Mana spent: " + str(data["manaspent"]),
-                                                                    True, (255, 255, 255))
+                                                                          True, (255, 255, 255))
     mana_rect = mana_text.get_rect(left=300, top=510)
     screen.blit(mana_text, mana_rect)
 
     # final health
     hp_text = pygame.font.Font('assets/spaceage.ttf', textsize1).render("Final health: " + str(data["hp"]), True,
-                                                                  (255, 255, 255))
+                                                                        (255, 255, 255))
     hp_rect = hp_text.get_rect(left=300, top=530)
     screen.blit(hp_text, hp_rect)
     pygame.display.flip()
